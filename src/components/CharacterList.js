c12b351ab01f60fa1d8ce26355b1df746ea998a7
Anthony Kawa <anthonyk2020@gmail.com>
2020-02-28 10:55:34 -0800

    Stretch Progress

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,26 +2,34 @@ import React, { useEffect, useState } from "react";
 import CharacterCard from './CharacterCard';
 import SearchForm from './SearchForm';
 import styled from 'styled-components';
+import {Pagination} from 'semantic-ui-react';
 import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
   const [search, setSearch] = useState('');
+  const [activePage, setActivePage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const [query, setQuery] = useState('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/');
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios.get(query)
+    axios.get(`${query}?page=${activePage}`)
     .then(res => {
       setCharacters(res.data.results);
+      setTotalPages(res.data.info.pages);
     })
     .catch(err => console.log(err));
-  }, [query]);
+  }, [activePage]);
 
   const updateSearch = (str) => {
     setSearch(str);
+  }
+
+  const handlePageChange = (e, { activePage }) => {
+    setActivePage(activePage);
   }
 
   return (
@@ -32,6 +40,7 @@ export default function CharacterList() {
         return <CharacterCard key={character.id} character={character} />
       }) : null}
       </ListWRapper>
+      <Pagination totalPages={totalPages} activePage={activePage} onPageChange={handlePageChange} />
     </section>
   );
 }
