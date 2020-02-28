7a5a84dd6a2b29d554e24b09fc8b24c2d664b0e4
Anthony Kawa <anthonyk2020@gmail.com>
2020-02-28 10:26:35 -0800

    MVP goals

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,29 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  return (
+        <Card>
+          <img src={character.image} />
+          <h2>{character.name}</h2>
+        </Card>
+  );
 }
+
+const Card = styled.div`
+  width: 30%;
+  text-align: center;
+  padding: 10px;
+
+  img {
+    width: 100%;
+    margin: 0;
+  }
+  
+  h2 {
+    background-color: #a9d3e9;
+    color: white;
+    margin: 0;
+    padding: 10px 0;
+  }
+`;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,43 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
+import styled from 'styled-components';
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/');
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get(query)
+    .then(res => {
+      setCharacters(res.data.results);
+    })
+    .catch(err => console.log(err));
+  }, [query]);
+
+  const updateSearch = (str) => {
+    setSearch(str);
+  }
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm updateSearch={updateSearch} />
+      <ListWRapper>
+      {characters.length > 0 ? characters.filter(character => character.name.toLowerCase().includes(search.toLowerCase())).map(character => {
+        return <CharacterCard key={character.id} character={character} />
+      }) : null}
+      </ListWRapper>
     </section>
   );
 }
+
+const ListWRapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
